bc9436f44777601280c5fd06d5825102cacc0f00
Martin Benninger <martin.benninger@outlook.com>
2018-11-16 17:22:08 +0100

    Add the webpack build

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const JazzUpdateSitePlugin = require('jazz-update-site-webpack-plugin');
+const moment = require('moment');
+const packageJson = require('./package.json');
+
+module.exports = (env) => {
+    const timestamp = moment().format('[_]YYYYMMDD[-]HHmm');
+    const version = (typeof env !== 'undefined' && (packageJson.version + "_" + env.buildUUID)) || packageJson.version + timestamp;
+    const config = {
+        entry: {
+            app: './index.js',
+        },
+        plugins: [
+            new JazzUpdateSitePlugin({
+                appType: 'ccm',
+                projectId: 'com.siemens.bt.jazz.workitemeditor.rtcEmailWorkItemAction',
+                acceptGlobPattern: [
+                    'resources/**',
+                    'META-INF/**',
+                    'plugin.xml',
+                ],
+                projectInfo: {
+                    author: packageJson.author,
+                    copyright: packageJson.author,
+                    description: packageJson.description,
+                    license: packageJson.license,
+                    version: version,
+                }
+            })
+        ]
+    };
+
+    return config;
+};
